450e00921bfb76c973bed48bef3c514667956071
Harsh Mangalam <harshdev8218@gmail.com>
2022-03-30 21:24:40 +0530

    fix cleanup issuess

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -4,8 +4,13 @@ export default function ThemeSwitcher() {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Button rounded={"$full"} size="sm" colorScheme={"warning"} onClick={toggleColorMode}>
-      {colorMode() === "light" ? "Night" : "Day"}
+    <Button
+      rounded={"$full"}
+      size="sm"
+      colorScheme={"warning"}
+      onClick={toggleColorMode}
+    >
+      {colorMode() === "light" ? "Light" : "Dark"} mode
     </Button>
   );
 }
--- src/hooks/useBattery.jsx
+++ src/hooks/useBattery.jsx
@@ -1,57 +1,56 @@
-import { onMount } from "solid-js";
+import { onCleanup, onMount } from "solid-js";
 import { createStore } from "solid-js/store";
 export default function useBattery() {
   const [store, setStore] = createStore({
     isSupported: true,
+    battery: null,
     charging: false,
     chargingTime: 0,
     dischargingTime: 0,
     level: 0,
   });
   onMount(async () => {
-    const battery = await navigator.getBattery();
-
-    if (!battery) {
+    try {
+      const battery = await navigator.getBattery();
+      setStore("battery", battery);
+      setStore("charging", battery.charging);
+      setStore("level", battery.level);
+      setStore("chargingTime", battery.chargingTime);
+      setStore("dischargingTime", battery.dischargingTime);
+      battery.addEventListener("chargingchange", updateChargeInfo);
+      battery.addEventListener("levelchange", updateLevelInfo);
+      battery.addEventListener("chargingtimechange", updateChargingInfo);
+      battery.addEventListener("dischargingtimechange", updateDischargingInfo);
+    } catch (error) {
+      console.log(error);
       setStore("isSupported", false);
-      return;
     }
-    updateAllBatteryInfo(battery);
-
-    battery.addEventListener("chargingchange", () => {
-      updateChargeInfo(battery);
-    });
-    battery.addEventListener("levelchange", () => {
-      updateLevelInfo(battery);
-    });
-    battery.addEventListener("chargingtimechange", () => {
-      updateChargingInfo(battery);
-    });
-    battery.addEventListener("dischargingtimechange", () => {
-      updateDischargingInfo(battery);
-    });
   });
 
-  function updateAllBatteryInfo(battery) {
-    updateChargeInfo(battery);
-    updateLevelInfo(battery);
-    updateChargingInfo(battery);
-    updateDischargingInfo(battery);
+  onCleanup(() => {
+    store.battery.removeEventListener("chargingchange", updateChargeInfo);
+    store.battery.removeEventListener("levelchange", updateLevelInfo);
+    store.battery.removeEventListener("chargingtimechange", updateChargingInfo);
+    store.battery.removeEventListener(
+      "dischargingtimechange",
+      updateDischargingInfo
+    );
+  });
+
+  function updateChargeInfo(e) {
+    setStore("charging", e.currentTarget.charging);
   }
 
-  function updateChargeInfo(battery) {
-    setStore("charging", battery.charging);
+  function updateLevelInfo(e) {
+    setStore("level", e.currentTarget.level);
   }
 
-  function updateLevelInfo(battery) {
-    setStore("level", battery.level);
+  function updateChargingInfo(e) {
+    setStore("chargingTime", e.currentTarget.chargingTime);
   }
 
-  function updateChargingInfo(battery) {
-    setStore("chargingTime", battery.chargingTime);
-  }
-
-  function updateDischargingInfo(battery) {
-    setStore("dischargingTime", battery.dischargingTime);
+  function updateDischargingInfo(e) {
+    setStore("dischargingTime", e.currentTarget.dischargingTime);
   }
 
   return {
